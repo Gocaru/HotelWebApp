13adcf34502a6266fa5ddd7b2c2b62ff4b48b715
Goncalo Russo <goncalorusso@gmail.com>
2025-07-24 17:58:21 +0100

    fix: Improve reservation details and edit pages UX
    The reservation edit page now disables all input fields and hides the 'Save' button for non-editable statuses (Completed, NoShow, etc.). The details page now shows a 'Process Payment' button for 'CheckedOut' reservations, improving the user workflow.

---
+++ HotelWebApp/Models/ReservationDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HotelWebApp.Data.Entities;
+
+namespace HotelWebApp.Models
+{
+    public class ReservationDetailsViewModel
+    {
+        public Reservation Reservation { get; set; }
+        public ChangeRequest? PendingChangeRequest { get; set; }
+    }
+}
